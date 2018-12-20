5078844eb8a45f3506662162308f43a1e1f4d1b2
Rachel Misbin <rachelmisbin@pitt.edu>
2018-12-19 21:22:40 -0500

    Modified test_processing.py to generate a pretty graph and used it to generate pretty graphs of the two last tests

--- test_processing.py
+++ test_processing.py
@@ -1,19 +1,23 @@
 #*** USING Python 3.6.6 ***
+# Make sure to install matplotlib if you want to be able to generate plots (pip3 install matplotlib)
 # for Python 2 input() = raw_input()
 # This script assumes that the static receiver latitude and longitude are stored at
 # the stop of the logfile in the following format:
 # 	Latitude: 39.9925
 # 	Longitude: -75.1415
 
+import matplotlib.pyplot as plt
+import numpy as np
 import math
 import sys
 from Haversine import Haversine
 
 def main():
-	data_points = 0
+	num_data_points = 0
+	data_points = []
 
 	logname = sys.argv[1]
-	outputFile = open(logname + "_processed", "x")
+	outputFile = open(logname + "_processed", "w")
 
 	inputFile = open(logname, "r")
 	for line in inputFile:
@@ -39,16 +43,30 @@ def main():
 			[tx_lat,tx_long]=to_DD(tx_lat,tx_long)
 
 			# Use first coordinates as starting coordinates
-			if (data_points == 0):
+			if (num_data_points == 0):
 				rx_lat = tx_lat
 				rx_long = tx_long
 
 			dist = calcDist(rx_lat, rx_long, tx_lat, tx_long)
 
 		if line.startswith('RSSI'):
-			data_points += 1
-			outputFile.write("Data point #" + str(data_points))
+			num_data_points += 1
+			outputFile.write("Data point #" + str(num_data_points))
 			outputFile.write("\nDistance: " + str(dist) + " m, " + line)
+			temp = line.split(' ')
+			data_points.append((float(dist), float(temp[1])))
+
+	distances, rssis = zip(*data_points)
+	fit = np.polyfit(distances, rssis, 1)
+	fit_fn = np.poly1d(fit)
+	fig = plt.figure()
+	plt.plot(distances,rssis, 'yo', distances, fit_fn(distances), '--k')
+	plt.plot(distances, rssis, 'ro')
+	plt.xlabel('Distance between transceivers (meters)')
+	plt.ylabel('Received signal strength (RSSI)')
+	plt.show()
+	fig.suptitle(logname)
+	fig.savefig(logname + "_plot.png")
 
 def hasFix(data):
 	return (data[2] and data[4] and data[8])
@@ -58,7 +76,7 @@ def hasFix(data):
 def calcDist(rx_lat, rx_long, tx_lat, tx_long):
 
 	# TODO: perform distance calculation
-	dist = Haversine([rx_long, rx_lat], [tx_long, tx_lat])
+	dist = Haversine([float(rx_long), float(rx_lat)], [float(tx_long), float(tx_lat)])
 	return dist.meters
 
 #Convert from dms to decimal degrees
